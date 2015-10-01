549c6930b766a5ce772bac84602d8897ab001ef6
Colin Rundel <rundel@gmail.com>
2015-10-01 19:35:58 -0400

    Fix file bug, switch to states instead of state

--- get_lq.R
+++ get_lq.R
@@ -6,27 +6,30 @@ listing_page = "/en/findandbook/hotel-listings.html"
 
 listings = read_html(paste0(base_url, listing_page))
 
-get_state_hotels = function(html, state, base_url, out_dir = "data/lq/")
+get_state_hotels = function(html, states, base_url, out_dir = "data/lq/")
 {
-  dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
-  
-  hotels = html_nodes(file, "#hotelListing a")
-  
-  # Find the start of a state's hotels by finding its name in the node text
-  # +2 to skip State and Back to Top anchor tags
-  start = 2+which(html_text(hotels) %>% str_trim() %in% paste("Hotels in", state))
-  stopifnot(length(start) == 1)
-  
-  # State label anchor have no href result in NA
-  urls = html_attr(hotels, "href")
-  label_index = which(is.na(urls))
-  end = label_index[label_index > start] %>% min() - 1
-  
-  for(url in urls[start:end])
+  for(state in states)
   {
-    download.file(paste0(base_url,url),
-                  destfile = paste0(out_dir,basename(url)),
-                  quiet = TRUE)
+    dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
+  
+    hotels = html_nodes(html, "#hotelListing a")
+    
+    # Find the start of a state's hotels by finding its name in the node text
+    # +2 to skip State and Back to Top anchor tags
+    start = 2+which(html_text(hotels) %>% str_trim() %in% paste("Hotels in", state))
+    stopifnot(length(start) == 1)
+    
+    # State label anchor have no href result in NA
+    urls = html_attr(hotels, "href")
+    label_index = which(is.na(urls))
+    end = label_index[label_index > start] %>% min() - 1
+    
+    for(url in urls[start:end])
+    {
+      download.file(paste0(base_url,url),
+                    destfile = paste0(out_dir,basename(url)),
+                    quiet = TRUE)
+    }
   }
 }
 
